8a3dec88ce3f050dbd5901b38f99b925731eae9d
moursalinme <me.moursalin@gmail.com>
2024-05-30 22:22:06 +0600

    smooth scrolling

--- src/js/script.js
+++ src/js/script.js
@@ -13,6 +13,7 @@ function scrollFunction() {
         header.classList.add("fixed");
         header.classList.add("ease-in-out");
         header.classList.add("transition-all");
+        header.classList.add("duration-700");
         header.classList.add("z-50");
         header.classList.add("shadow-xl");
         header.classList.add("shadow-gray-100");
@@ -21,6 +22,7 @@ function scrollFunction() {
         header.classList.remove("ease-in-out");
         header.classList.remove("transition-all");
         header.classList.remove("z-50");
+        header.classList.remove("duration-700");
         header.classList.remove("shadow-xl");
         header.classList.remove("shadow-gray-100");
     }
@@ -29,6 +31,14 @@ function scrollFunction() {
 document.getElementById('explore-site').addEventListener('click', function(event) {
     event.preventDefault(); 
     document.getElementById('cometome').scrollIntoView({ behavior: 'smooth', duration: '500' });
+});
+
+document.getElementById('favname').addEventListener('click', function(event) {
+    event.preventDefault(); 
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 });
 
 document.getElementById('clearmsg').addEventListener('click', function() {
